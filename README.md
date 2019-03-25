3788711675a387f8708b9d77342fb3ae8b7986bc
OhadR-Cellebrite-PC <ohad.redlich@cellebrite.com>
2019-03-25 17:19:43 +0200

    no message

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ReloadableProperties
+
+
+
+mvn clean tomcat7:run -Dohadr.project.port=8093
+
+
+## Debug within Eclipse
+
+See how in this README: https://gitlab.com/OhadR/activemq-spring-sandbox#debug-within-eclipse
+
--- src/main/java/com/ohadr/ae_case_analysis/service/MyService.java
+++ src/main/java/com/ohadr/ae_case_analysis/service/MyService.java
@@ -1,6 +1,5 @@
 package com.ohadr.ae_case_analysis.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import com.morgan.design.properties.ReloadableProperty;
@@ -11,16 +10,19 @@ public class MyService {
 
 	@ReloadableProperty("dynamicProperty.longValue")
 	private long primitiveWithDefaultValue = 55;
-	
-	@ReloadableProperty("dynamicProperty.substitutionValue")
-	private String stringProperty;
-	
-	@ReloadableProperty("dynamicProperty.compoiteStringValue")
-	private String compsiteStringProperty;
+
+	@ReloadableProperty("dynamicProperty.stringValue")
+	private String stringValue;
+
+//	@ReloadableProperty("dynamicProperty.substitutionProperty")
+//	private String stringProperty;
+//	
+//	@ReloadableProperty("dynamicProperty.compoiteStringValue")
+//	private String compsiteStringProperty;
 
 
 	public String getStringValue()
 	{
-		return stringProperty;
+		return stringValue;
 	}
 }
