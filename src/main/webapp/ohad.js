8e88154644db116b54947ebe218862475387dac3
OhadR-Cellebrite-PC <ohad.redlich@cellebrite.com>
2019-03-26 09:42:24 +0200

    UI... minor

--- src/main/webapp/ohad.js
+++ src/main/webapp/ohad.js
@@ -1,35 +1,21 @@
-var CONNECT_MONGODB_AND_CASE_URL = './connectToMongoServerAndGetCases';
-var EXTRACTIONS_URL =      './extractions';
-
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart', 'table']});
-
-// Set a callback to run when the Google Visualization API is loaded.
-//google.charts.setOnLoadCallback(drawChart);
+var PROPERTIES_URL =      './properties';
 
 var mongoClusterData;
-var mongoClusterTable;
 
 function populateServerData( mongoServer )
 {
 	$("#server_name").text( mongoServer );
 }
 
-function initCasesTable() 
-{
-    mongoClusterTable = new google.visualization.Table(document.getElementById('cases_table_div'));
-}
-
-      
 $(document).ready(function() {
 	$("#submit").click(function(){
 		
-		callBackendConnectAndGetCases();
+		callBackendGetProperties();
 	});
 });
 
 
-function callBackendConnectAndGetCases()
+function callBackendGetProperties()
 {
 	var serverAddress =	$("#server_name_input").val();
 
@@ -38,8 +24,8 @@ function callBackendConnectAndGetCases()
 		};
 
 	$.ajax({
-		url: CONNECT_MONGODB_AND_CASE_URL,
-		type: 'POST',
+		url: PROPERTIES_URL,
+		type: 'GET',
 		data: requestData,
         dataType: 'text',    //The type of data that you're expecting back from the server
         contentType: 'application/x-www-form-urlencoded',    //When sending data to the server, use this content type
@@ -52,68 +38,4 @@ function callBackendConnectAndGetCases()
 			alert('could not connect to mongo ' + serverAddress)
 		}
 	});
-}
-
-
-/*
- * the json is in this format:
- * "[{"_id":"6234854c-b7c0-470f-a3c3-131aaaa947b8","name":"New case01","status":"Active"},{"_id":"ca5188b0-0fdd-4966-949f-59f9d39c08b5","name":"New case02","status":"Active"}]"
- */
-function drawCasesOnGrid(cases) {
-
-	//first of all, clear the table from previous cases:
-    mongoClusterData = new google.visualization.DataTable();
-    mongoClusterData.addColumn('string', 'Name');
-    mongoClusterData.addColumn('string', 'CaseId');
-//    mongoClusterData.addColumn('boolean', 'Primary');
-    mongoClusterData.addColumn('string', 'Status');
-
-
-	for (var i in cases)
-	{
-		var node = cases[i];
-		//find the node in the table:
-		var rows = mongoClusterData.getFilteredRows([{column: 0, value: node.name}]);
-		if(rows.length !== 0) //if we did not find by filter, create a new row
-		{
-			var rowIndex = rows[0];	//expect a single row with this server name;
-			mongoClusterData.setCell(rowIndex, 1, node._id);
-			mongoClusterData.setCell(rowIndex, 2, node.status);
-		}
-		else
-		{
-		    mongoClusterData.addRow(
-		        [node.name, node._id, node.status]  );
-		}
-
-		mongoClusterTable.draw(mongoClusterData, {showRowNumber: true});
-
-	}
-}
-
-function exportExtractionsMetricsForSelectedCase()
-{
-	var selections = mongoClusterTable.getSelection();
-	if(selections.length === 0)
-		return;
-	var caseId = mongoClusterData.getFormattedValue(selections[0].row, 1);
-	var requestData = {
-			caseId: caseId
-		};
-
-	$.ajax({
-		url: EXTRACTIONS_URL,
-        type: 'POST',
-		data: requestData,
-//        dataType: 'text',    //The type of data that you're expecting back from the server
-        contentType: 'application/x-www-form-urlencoded',    //When sending data to the server, use this content type
-		success: function(response, textStatus, jqXHR){
-			var cases = response;
-			window.open('./outputs/' + caseId + '.csv');
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			//do something?
-		}
-	});
-	
 }
